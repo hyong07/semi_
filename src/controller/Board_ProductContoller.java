4bf92f066bd36fedf55c4609e5626cddd63c1e67
dannyktv <dhkdqjtjteorkf@gmail.com>
2018-06-22 14:11:11 +0900

    boardController 주석
    ㅇ

--- src/controller/Board_ProductContoller.java
+++ src/controller/Board_ProductContoller.java
@@ -10,6 +10,23 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("*.bo")
 public class Board_ProductContoller extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String requestURI = request.getRequestURI();
+			String contextPath = request.getContextPath();
+			String command = requestURI.substring(contextPath.length());
+			/*
+			 * 
+			 * 카테고리 
+			 * 패션카테고리를 누를시 보드(게시판)에서 카테고리가 패션인 애들만 소환
+			 * 
+			 * 
+			 */
+			
+				
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
